e0d8d8c00dca87fc0c86b7dd9ef5b7ec1d67da78
student <iliyapolyakov2006@gmail.com>
2024-03-06 15:47:41 +0300

    Задача 4

---
+++ 4.py
@@ -0,0 +1,19 @@
+import csv
+import random
+from string import ascii_letters,digits
+
+
+with open("scientist.txt", encoding="utf-8") as f:
+    s = list(csv.DictReader(f, delimiter="#"))
+    for i in s:
+        name = i["ScientistName"].split()
+        i["login"] = name[0]+"_"+""+name[1][0]+name[2][0]
+        i["password"] = random.choices(ascii_letters, k=10) + random.choices(digits, k=10)
+        random.shuffle(i["password"])
+        i["password"] = "".join(i["password"])
+
+
+with open("scientist_password.csv", "w", encoding="utf-8", newline="") as f:
+    writer = csv.DictWriter(f, fieldnames=["ScientistName","preparation", "date", "components", "login", "password"], delimiter = "#")
+    writer.writeheader()
+    writer.writerows(s)

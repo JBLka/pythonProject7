0e3a122c651b72b80ef99e56effc56bc726a447e
student <iliyapolyakov2006@gmail.com>
2024-03-06 16:07:38 +0300

    Задача 5

--- 5.py
+++ 5.py
@@ -1,6 +1,6 @@
 import csv
 import random
-from string import ascii_letters, digits
+
 
 def hash(s):
     a = list(range(1024))
@@ -8,6 +8,7 @@ def hash(s):
     s = s.split()
     s = [ord(x[0]) % 1024 for x in s]
     return a[s[0]] + a[s[1]] + a[s[2]]
+
 
 with open("scientist.txt", encoding="utf-8") as f:
     s = list(csv.DictReader(f, delimiter="#"))

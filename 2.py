e03f1fe45d2b81dc7a7256f4167da3ccdb7169bd
student <iliyapolyakov2006@gmail.com>
2024-03-06 15:09:57 +0300

    Задача 2

--- 2.py
+++ 2.py
@@ -1,11 +1,30 @@
 import csv
 import datetime
+import random
 
+def quicksort(n):
+    if len(n) <= 1:
+        return n
+    else:
+        l_list = []
+        m_list = []
+        r_list = []
+        x = random.choice([j for j in n])
+        q = datetime.date(list(map(int, x["date"].split("-")))[0], list(map(int, x["date"].split("-")))[1], list(map(int, x["date"].split("-")))[2])
+        for z in n:
+            b = datetime.date(list(map(int, z["date"].split("-")))[0], list(map(int, z["date"].split("-")))[1], list(map(int, z["date"].split("-")))[2])
+            if b < q:
+                l_list.append(z)
+            elif b > q:
+                r_list.append(z)
+            else:
+                m_list.append(z)
+        return quicksort(l_list) + m_list + quicksort(r_list)
 
 with open("scientist.txt", encoding="utf-8") as f:
     s = list(csv.DictReader(f, delimiter="#"))
     scientists = {}
-    s = sorted(s, key=lambda x: datetime.date(list(map(int, x["date"].split("-")))[0], list(map(int, x["date"].split("-")))[1], list(map(int, x["date"].split("-")))[2]))
+    s = quicksort(s)
 
 with open("scientist.txt", "w", encoding="utf-8", newline="") as f:
     writer = csv.DictWriter(f, fieldnames=["ScientistName","preparation", "date", "components"], delimiter = "#")

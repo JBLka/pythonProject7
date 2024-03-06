6bcafc19a3946b4b725ffe6354d8f165a044a535
student <iliyapolyakov2006@gmail.com>
2024-03-06 16:18:27 +0300

    Задача 3

--- 3.py
+++ 3.py
@@ -3,13 +3,25 @@ import datetime
 
 
 with open("scientist.txt", encoding="utf-8") as f:
-    s = list(csv.DictReader(f, delimiter="#"))
-    t = input()
-    while t != "эксперимент":
-        dd, mm, yyyy = map(int, t.split("."))
-        date = str(datetime.date(yyyy, mm, dd))
-        for i in s:
-            if i["date"] == date:
-                name = i["ScientistName"].split()
-                print(f'Ученый {name[0]} {name[1][0]}. {name[2][0]}. создал препарат: {i["preparation"]} - {date}')
-        t = input()
+    reader = list(csv.DictReader(f, delimiter="#"))
+    request = input()
+    while request != "эксперимент":
+        request = request.split('.')
+        request = request[::-1]
+        request = str(datetime.date(int(request[0]), int(request[1]), int(request[2])))
+
+        left = 0
+        right = len(reader) - 1
+        while left <= right:
+            center = (left + right) // 2
+            if request == reader[center]['date']:
+                scientist = reader[center]
+                scientist_name = scientist['ScientistName'].split()
+                print(f'Ученый {scientist_name[0]} {scientist_name[1][0]}.{scientist_name[2][0]}.'
+                      f' создал препарат: {scientist["preparation"]} - {scientist["date"]}')
+                break
+            if request > reader[center]['date']:
+                left = center + 1
+            else:
+                right = center - 1
+        request = input()

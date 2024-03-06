49dfff12168fd61cef33f1406418862f8bebbe7c
student <iliyapolyakov2006@gmail.com>
2024-03-06 14:16:57 +0300

    Задача 1

--- 1.py
+++ 1.py
@@ -23,18 +23,3 @@ with open("scientist_origin.txt", "w", encoding="utf-8", newline="") as f:
     writer = csv.DictWriter(f, fieldnames=["ScientistName","preparation", "date", "components"], delimiter = "#")
     writer.writeheader()
     writer.writerows(scientists)
-
-
-
-    # for i in s:
-    #     data = datetime.date(map(int, i["date"].split("-")))
-    #     if len(new_f) == 0:
-    #         new_f.append(i)
-    #     elif
-        # if i["preparation"] == "Аллопуринол":
-        #     print(i["ScientistName"])
-        # elif i["preparation"] not in scientists.keys():
-        #     scientists[i["preparation"]] = i
-        # elif int(scientists[i["preparation"]]["date"][:4]) > int(i["date"][:4]):
-        #     scientists[i["preparation"]] = i
-    # print(scientists)

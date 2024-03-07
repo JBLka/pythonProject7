1d8f7b841685653a095508ba7dd4e52ec33a9a68
student <iliyapolyakov2006@gmail.com>
2024-03-07 10:15:54 +0300

    Задача 4

--- 4.py
+++ 4.py
@@ -6,10 +6,8 @@ with open("scientist.txt", encoding="utf-8") as f:
     s = list(csv.DictReader(f, delimiter="#"))
     for i in s:
         name = i["ScientistName"].split()
-        i["login"] = name[0] + "_" + "" + name[1][0] + name[2][0]
-        i["password"] = random.choices(ascii_letters + digits, k=10)
-        random.shuffle(i["password"])
-        i["password"] = "".join(i["password"])
+        i["login"] = name[0] + "_" + "" + name[1][0] + name[2][0]              # Генерируем логин по ФИО
+        i["password"] = "".join(random.choices(ascii_letters + digits, k=10))  # Генерируем пароль
 
 with open("scientist_password.csv", "w", encoding="utf-8", newline="") as f:
     writer = csv.DictWriter(f, fieldnames=["ScientistName", "preparation", "date", "components", "login", "password"],

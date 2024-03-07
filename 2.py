34096e5a022e87fa8b88273d6929b33f39677bef
student <iliyapolyakov2006@gmail.com>
2024-03-07 10:15:46 +0300

    Задача 2

--- 2.py
+++ 2.py
@@ -3,7 +3,7 @@ import datetime
 import random
 
 
-def quicksort(n):
+def quicksort(n):                                     # Сортировка
     if len(n) <= 1:
         return n
     else:
@@ -11,23 +11,22 @@ def quicksort(n):
         m_list = []
         r_list = []
         x = random.choice([j for j in n])
-        q = datetime.date(list(map(int, x["date"].split("-")))[0], list(map(int, x["date"].split("-")))[1],
-                          list(map(int, x["date"].split("-")))[2])
+        date = list(map(int, x["date"].split("-")))
+        q = datetime.date(date[0], date[1], date[2])  # Случайная дата из списка
         for z in n:
-            b = datetime.date(list(map(int, z["date"].split("-")))[0], list(map(int, z["date"].split("-")))[1],
-                              list(map(int, z["date"].split("-")))[2])
+            date = list(map(int, z["date"].split("-")))
+            b = datetime.date(date[0], date[1], date[2])
             if b < q:
-                l_list.append(z)
+                l_list.append(z)                      # Открытия в этот день
             elif b > q:
-                r_list.append(z)
+                r_list.append(z)                      # Открытия в последующие дни
             else:
-                m_list.append(z)
+                m_list.append(z)                      # Открытия в предыдущие дни
         return quicksort(l_list) + m_list + quicksort(r_list)
 
 
 with open("scientist.txt", encoding="utf-8") as f:
     s = list(csv.DictReader(f, delimiter="#"))
-    scientists = {}
     s = quicksort(s)
 
 with open("scientist.txt", "w", encoding="utf-8", newline="") as f:

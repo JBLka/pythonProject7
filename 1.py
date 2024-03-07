68415c7b8b6bce6e7594ef64fd7f37689887ac28
student <iliyapolyakov2006@gmail.com>
2024-03-07 10:15:42 +0300

    Задача 1

--- 1.py
+++ 1.py
@@ -4,23 +4,23 @@ import datetime
 with open("scientist.txt", encoding="utf-8") as f:
     s = list(csv.DictReader(f, delimiter="#"))
     scientists = {}
-    new_f = []
     c = ""
     s = sorted(s, key=lambda x: datetime.date(list(map(int, x["date"].split("-")))[0],
                                               list(map(int, x["date"].split("-")))[1],
                                               list(map(int, x["date"].split("-")))[2]))
+    # Отсортировать список лекарств по дате
     print("Разработчиками Аллопуринола были такие люди")
     for i in s:
-        if i["preparation"] not in scientists.keys():
+        if i["preparation"] not in scientists.keys():  # Добавить неуспользуемый элемент в список
             scientists[i["preparation"]] = i
-        if i["preparation"] == "Аллопуринол":
+        if i["preparation"] == "Аллопуринол":          # Вывести создателей аллопуринола
             if c == "":
                 c = i["ScientistName"]
             print(i["ScientistName"] + " - " + i["date"])
     print(f'Оригинальный рецепт принадлежит: {i["ScientistName"]}')
     scientists = list(scientists.values())
 
-with open("scientist_origin.txt", "w", encoding="utf-8", newline="") as f:
+with open("scientist_origin.txt", "w", encoding="utf-8", newline="") as f:  # Файл с оригинальными авторами
     writer = csv.DictWriter(f, fieldnames=["ScientistName", "preparation", "date", "components"], delimiter="#")
     writer.writeheader()
     writer.writerows(scientists)

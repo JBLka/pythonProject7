86ad929097ba66baa66bccb8de166657cf978a47
student <iliyapolyakov2006@gmail.com>
2024-03-07 09:02:41 +0300

    Задача 1

--- 1.py
+++ 1.py
@@ -1,13 +1,14 @@
 import csv
 import datetime
 
-
 with open("scientist.txt", encoding="utf-8") as f:
     s = list(csv.DictReader(f, delimiter="#"))
     scientists = {}
     new_f = []
     c = ""
-    s = sorted(s, key=lambda x: datetime.date(list(map(int, x["date"].split("-")))[0], list(map(int, x["date"].split("-")))[1], list(map(int, x["date"].split("-")))[2]))
+    s = sorted(s, key=lambda x: datetime.date(list(map(int, x["date"].split("-")))[0],
+                                              list(map(int, x["date"].split("-")))[1],
+                                              list(map(int, x["date"].split("-")))[2]))
     print("Разработчиками Аллопуринола были такие люди")
     for i in s:
         if i["preparation"] not in scientists.keys():
@@ -15,11 +16,11 @@ with open("scientist.txt", encoding="utf-8") as f:
         if i["preparation"] == "Аллопуринол":
             if c == "":
                 c = i["ScientistName"]
-            print(i["ScientistName"]+" - "+i["date"])
+            print(i["ScientistName"] + " - " + i["date"])
     print(f'Оригинальный рецепт принадлежит: {i["ScientistName"]}')
     scientists = list(scientists.values())
 
 with open("scientist_origin.txt", "w", encoding="utf-8", newline="") as f:
-    writer = csv.DictWriter(f, fieldnames=["ScientistName","preparation", "date", "components"], delimiter = "#")
+    writer = csv.DictWriter(f, fieldnames=["ScientistName", "preparation", "date", "components"], delimiter="#")
     writer.writeheader()
     writer.writerows(scientists)
